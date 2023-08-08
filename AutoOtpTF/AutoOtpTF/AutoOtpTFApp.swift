c3ed30f8acc01fa2a51c2a51e08c8d88d32b032f
TyroneSong <songpu_work@163.com>
2023-08-08 09:48:52 +0800

    Add AutoOtpTF. Compatible 15&16 Xcode14

---
+++ AutoOtpTF/AutoOtpTF/AutoOtpTFApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoOtpTFApp.swift
+//  AutoOtpTF
+//
+//  Created by 宋璞 on 2023/8/7.
+//
+
+import SwiftUI
+
+@main
+struct AutoOtpTFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
