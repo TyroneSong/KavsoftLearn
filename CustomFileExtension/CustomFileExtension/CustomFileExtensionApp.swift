595af99ff9f5ddee990e335118e2d88d243a0a5a
TyroneSong <songpu_work@163.com>
2024-01-10 15:09:13 +0800

    CustomFileExtension

---
+++ CustomFileExtension/CustomFileExtension/CustomFileExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFileExtensionApp.swift
+//  CustomFileExtension
+//
+//  Created by 宋璞 on 2024/1/10.
+//
+
+import SwiftUI
+
+@main
+struct CustomFileExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
