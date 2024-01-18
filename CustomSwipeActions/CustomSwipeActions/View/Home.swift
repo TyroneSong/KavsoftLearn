5f2fc31e175d7aa336982b78b93029b679372945
TyroneSong <songpu_work@163.com>
2024-01-18 14:37:36 +0800

    CustomSwipeActions

--- CustomSwipeActions/CustomSwipeActions/View/Home.swift
+++ CustomSwipeActions/CustomSwipeActions/View/Home.swift
@@ -8,13 +8,57 @@
 import SwiftUI
 
 struct Home: View {
+    /// Sample Array of Colors
+    @State private var colors: [Color] = [.black, .yellow, .purple, .brown]
+    
     var body: some View {
         ScrollView(.vertical) {
-            
+            LazyVStack(spacing: 10) {
+                ForEach(colors, id:\.self) { color in
+                    SwipeAction(cornerRadius: 15, direction: .trailing) {
+                        CardView(color)
+                    } actions: {
+                        Action(tint: .blue, icon: "star.fill", isEnabled: color != .black) {
+                            print("BookMark")
+                        }
+                        
+                        Action(tint: .red, icon: "trash.fill") {
+                            withAnimation(.easeInOut) {
+                                colors.removeAll(where: { $0 == color })
+                            }
+                        }
+                    }
+
+                }
+            }
+            .padding(15)
         }
         .scrollIndicators(.hidden)
     }
+    
+    @ViewBuilder
+    func CardView(_ color: Color) -> some View {
+        HStack(spacing: 12) {
+            Circle()
+                .frame(width: 50, height: 50)
+            
+            VStack(alignment: .leading, spacing: 6, content: {
+                RoundedRectangle(cornerRadius: 5)
+                    .frame(width: 80, height: 6)
+                RoundedRectangle(cornerRadius: 5)
+                    .frame(width: 60, height: 6)
+            })
+            
+            Spacer(minLength: 0)
+        }
+        .foregroundStyle(.white.opacity(0.4))
+        .padding(.horizontal, 15)
+        .padding(.vertical, 10)
+        .background(color.gradient)
+        
+    }
 }
+
 
 #Preview {
     ContentView()
